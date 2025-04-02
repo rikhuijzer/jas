74eb87e6d5ff84340ed8ee4170a18e6a5144f993
Rik Huijzer <github@huijzer.xyz>
2025-04-02 17:59:58 +0200

    Install from url

--- tests/test_install.rs
+++ tests/test_install.rs
@@ -31,3 +31,37 @@ fn test_install_gh() {
         .success()
         .stdout(predicate::str::contains("1.31.1"));
 }
+
+#[test]
+fn test_install_url() {
+    let base = "https://www.johnvansickle.com/ffmpeg/old-releases/";
+    let url = if cfg!(target_os = "macos") && cfg!(target_arch = "aarch64") {
+        format!("{base}/ffmpeg-6.0.1-arm64-static.tar.xz")
+    } else if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") {
+        format!("{base}/ffmpeg-6.0.1-amd64-static.tar.xz")
+    } else {
+        return;
+    };
+    let mut cmd = bin();
+    cmd.arg("--verbose")
+        .arg("install")
+        .arg("--url")
+        .arg(&url)
+        .arg("--dir")
+        .arg("tests")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(url))
+        .stdout(predicate::str::contains(
+            "you may need to add it to your PATH manually",
+        ));
+    let path = std::path::Path::new("tests/ffmpeg");
+    assert!(path.exists());
+
+    let mut version_cmd = Command::new(path);
+    version_cmd.arg("--version");
+    version_cmd
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("6.0.1"));
+}
