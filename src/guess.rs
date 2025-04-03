da1186a4bcea12dd799eaca60a380c43412e6e2a
Rik Huijzer <github@huijzer.xyz>
2025-04-03 07:04:12 +0200

    Allow guessing amd64

--- src/guess.rs
+++ src/guess.rs
@@ -9,20 +9,27 @@ enum TargetOs {
 enum TargetArch {
     X86_64,
     Aarch64,
+    Arm,
+}
+
+fn contains_x86_64(name: &str) -> bool {
+    name.contains("x86_64") || name.contains("amd64")
 }
 
 fn guess_asset_enum(names: &[&str], target_os: TargetOs, target_arch: TargetArch) -> usize {
     let searcher = |name: &&str| {
         if target_os == TargetOs::Linux && target_arch == TargetArch::X86_64 {
-            name.contains("linux") && name.contains("x86_64")
+            name.contains("linux") && contains_x86_64(name)
         } else if target_os == TargetOs::Macos && target_arch == TargetArch::X86_64 {
-            (name.contains("macos") || name.contains("darwin")) && name.contains("x86_64")
+            (name.contains("macos") || name.contains("darwin")) && contains_x86_64(name)
         } else if target_os == TargetOs::Macos && target_arch == TargetArch::Aarch64 {
             (name.contains("macos") || name.contains("darwin")) && name.contains("aarch64")
         } else if target_os == TargetOs::Linux && target_arch == TargetArch::Aarch64 {
             name.contains("linux") && name.contains("aarch64")
+        } else if target_os == TargetOs::Linux && target_arch == TargetArch::Arm {
+            name.contains("linux") && name.contains("arm")
         } else if target_os == TargetOs::Windows && target_arch == TargetArch::X86_64 {
-            name.contains("windows") && name.contains("x86_64")
+            name.contains("windows") && contains_x86_64(name)
         } else {
             panic!("Unsupported platform: {}", name);
         }
@@ -45,6 +52,8 @@ pub fn guess_asset(names: &[&str]) -> usize {
         TargetArch::X86_64
     } else if cfg!(target_arch = "aarch64") {
         TargetArch::Aarch64
+    } else if cfg!(target_arch = "arm") {
+        TargetArch::Arm
     } else {
         panic!("Unsupported architecture");
     };
@@ -66,4 +75,17 @@ fn test_guess_asset_typos() {
     assert_eq!(index, 4);
     let index = guess_asset_enum(&names, TargetOs::Windows, TargetArch::X86_64);
     assert_eq!(index, 3);
+}
+
+#[test]
+fn test_guess_asset_pandoc() {
+    let names = vec![
+        "pandoc-3.6.4-1-amd64.deb",
+        "pandoc-3.6.4-linux-amd64.tar.gz",
+        "pandoc-3.6.4-linux-arm64.tar.gz",
+    ];
+    let index = guess_asset_enum(&names, TargetOs::Linux, TargetArch::X86_64);
+    assert_eq!(index, 1);
+    let index = guess_asset_enum(&names, TargetOs::Linux, TargetArch::Arm);
+    assert_eq!(index, 2);
 }
